d4532cf110bf6bd0fbc7c8a7e2556b6075e47333
ZX <87819234+ZXASQWEDF@users.noreply.github.com>
2021-12-14 23:21:22 +0800

    Add files via upload

---
+++ djskamj.js
@@ -0,0 +1,9 @@
+"use strict";
+x = 3.14; // 报错 (x 未定义)
+let xxx = window.confirm("以停服，系統有大升級")
+if (xxx) {
+    window.alert("你好！");
+} else { window.location.href = "https://zxasqwedf.github.io/1/登入.html"; }
+loginButton.addEventListener('click', login) // JavaScript Document
+var flag = false;
+var div = document.getElementById("con");
